f1be83232e549173225e11852ab0a6c22e1f8626
fomenkoyegor <fomenkoyegor@gmail.com>
2019-10-03 21:29:26 +0300

    change fix

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,44 @@ import './js/index.js';
 import './index.scss';
 
 class Post {
-    constructor (options = {}) {
+    constructor(options = {}) {
+        this.elem = null;
+        this.btnInfo = null;
         this.id = options.item.id;
         this.name = options.item.name;
-        console.log(options);
+
+        this.eventsAttach();
     }
-    create () {
-        const elem = document.createElement('div');
-        elem.dataset.id = this.id;
-        elem.textContent = this.name;
-        return elem;
+
+    eventsAttach(){
+       
     }
+
+    getInfo(){
+        const info =  {
+            'id': this.id,
+            name: this.name,
+            
+        };
+        console.log(info)
+    }
+
+    create() {
+        this.elem = document.createElement('div');
+        this.elem.dataset.id = this.id;
+        this.elem.textContent = this.name;
+        this.elem.append(this.createBtnInfo());
+        return this.elem;
+    }
+
+    createBtnInfo(){
+        this.btnInfo = document.createElement('button');
+        this.btnInfo.textContent='info';
+        this.btnInfo.addEventListener('click',this.getInfo.bind(this));
+        return this.btnInfo;
+    }
+
+
 }
 
 // const obj = {
@@ -23,11 +50,11 @@ class Post {
 // new Post ({item: obj});
 
 fetch('api/test').then(res => res.json()).then(res => {
-    // console.log(res);
-    if (res && res.length) {
-        res.forEach(elem => {
-            let post = new Post({item: elem});
-            console.log(post)
-        })
+    if (res) {
+        (res.data).forEach(elem => {
+            let post = new Post({item : elem});
+            document.body.append(post.create())
+        
+        });
     };
 });
