361e5b5c9017acee1825fb5c138c69cdd636d8c1
fomenkoyegor <fomenkoyegor@gmail.com>
2019-10-03 20:19:35 +0300

    add route TESTROUTE=>getByID

--- src/routes/test.js
+++ src/routes/test.js
@@ -3,12 +3,20 @@ var router = express.Router();
 
 
 const data = [
-    {id:0,name:"vasya"},
-    {id:1,name:"petya"},
+    { id: 'fjbaskjnfkjanfkjnasfnas', name: "vasya" },
+    { id: '1212121211asds1da5s1dasd', name: "petya" },
+    { id: 'sfasfkjas712647124912', name: "reqwq" },
+    { id: "nkjsdnfjksndfkjnsdkjfnsdfnskjd", name: "zxzxc" },
 ];
 
-router.get('/', function(req, res, next) {
-  res.status(200).send({data});
+router.get('/', function (req, res, next) {
+    res.status(200).send({ data });
+});
+
+router.get('/:id', function (req, res) {
+    const id = req.params.id;
+    const item = data.find(el=>el.id==id);
+    res.status(201).send(item)
 });
 
 export default router;
